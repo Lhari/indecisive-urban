8fcf129bd860d62bc3c9fa36e2770ddad3455a04
Andrew Macnaughton <andrew.macnaughton@theiggroup.com>
2016-07-12 01:56:59 +0100

    moved css folder into source directory

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var
+	gulp 		= require('gulp'),
+    concat      = require('gulp-concat'),
+    sass        = require('gulp-sass'),
+    uglify      = require('gulp-uglify'),
+    watch       = require('gulp-watch'),
+    prefix      = require('gulp-autoprefixer')
+;
+
+
+gulp.task('css', function() {
+  gulp.src('source/styles/**.scss')
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(prefix("last 3 versions", "> .5%", "ie9")
+      .on('error', function (error) { console.warn(error.message); }))
+    .pipe(gulp.dest('template/css/'))
+});
+
+gulp.task('fonts', function() {
+  gulp.src('source/font/*')
+    .pipe(gulp.dest('template/css/font'))
+});
+
+
+
+gulp.task('watch', function() {
+      gulp.watch('source/styles/*/**', ['css']);
+})
+
+gulp.task('default', ['css', 'watch'], function() {});
