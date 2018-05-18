d0ec4dcd7705490c678039285d8feda72411c3e7
Andrew Macnaughton <andrew.macnaughton@theiggroup.com>
2018-05-18 14:59:23 +0100

    created BFA theme and fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,47 @@
-var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  sass = require('gulp-sass'),
-  cssnano = require('gulp-cssnano'),
-  prefix = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps'),
-  jshint = require('gulp-jshint'),
-  stylish = require('jshint-stylish'),
-  uglify = require('gulp-uglify'),
-  concat = require('gulp-concat'),
-  rename = require('gulp-rename'),
-  plumber = require('gulp-plumber'),
-  bower = require('gulp-bower');
+const gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	prefix = require('gulp-autoprefixer'),
+	sass = require('gulp-sass')
 
-gulp.task('css', function() {
-  gulp.src('source/styles/**.*css')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(prefix("last 3 versions", "> .5%", "ie9")
-      .on('error', function(error) {
-        console.warn(error.message);
-      }))
-    .pipe(gulp.dest('template/css/'))
+gulp.task('css', function () {
+	gulp.src('source/styles/**.*css')
+		.pipe(sass({
+			errLogToConsole: true
+		}))
+		.pipe(prefix("last 3 versions", "> .5%", "ie9")
+			.on('error', function (error) {
+				console.warn(error.message);
+			}))
+		.pipe(gulp.dest('template/css/'))
 });
 
-gulp.task('fonts', function() {
-  gulp.src('source/font/*')
-    .pipe(gulp.dest('template/css/font'))
+gulp.task('fonts', function () {
+	gulp.src('source/font/*')
+		.pipe(gulp.dest('template/css/font'))
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('source/scripts/*.js')
-   // .pipe(plumber())
-   // .pipe(jshint())
-   // .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('template/scripts'))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('template/scripts'))
+gulp.task('scripts', function () {
+	return gulp.src('source/scripts/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('template/scripts'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('template/scripts'))
 });
 
-gulp.task('root', function() {
-  gulp.src('root/*.php').pipe(gulp.dest('..'));
+gulp.task('root', function () {
+	gulp.src('root/*.php').pipe(gulp.dest('..'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('source/styles/*/**', ['css']);
-  gulp.watch('source/scripts/*.js', ['scripts']);
-  gulp.watch('source/font/*/**', ['fonts']);
-  gulp.watch('root/*.php', ['root'])
+gulp.task('watch', function () {
+	gulp.watch('source/styles/*/**', ['css']);
+	gulp.watch('source/scripts/*.js', ['scripts']);
+	gulp.watch('source/font/*/**', ['fonts']);
+	gulp.watch('root/*.php', ['root'])
 });
 
-gulp.task('default', ['css', 'scripts', 'fonts', 'root', 'watch'], function() {});
+gulp.task('default', ['css', 'scripts', 'fonts', 'root', 'watch'], function () {});
